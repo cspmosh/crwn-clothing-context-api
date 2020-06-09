bad2975d25d7005eb1d90dee183252f76485b30f
Josh Turner <cs.pmosh@gmail.com>
2020-06-09 15:54:05 -0500

    convert cart components to using context API instead of redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyAM8QI4FiUOGJTyZtxMQaCQ26z2WwH8OvQ",
+  authDomain: "crwn-db-12037.firebaseapp.com",
+  databaseURL: "https://crwn-db-12037.firebaseio.com",
+  projectId: "crwn-db-12037",
+  storageBucket: "crwn-db-12037.appspot.com",
+  messagingSenderId: "267683877909",
+  appId: "1:267683877909:web:c2a277206cee62c0006a1c",
+  measurementId: "G-N8DMGDC50N"
 };
 
 firebase.initializeApp(config);
